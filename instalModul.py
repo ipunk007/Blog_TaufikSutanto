2a21503c4545136854dfc5721ef29d0f658e2a7c
Taufik Sutanto <taufikedys@users.noreply.github.com>
2018-01-12 00:34:04 +0700

    Update instalModul.py

--- instalModul.py
+++ instalModul.py
@@ -4,13 +4,13 @@ Created on Fri Jan  5 15:03:38 2018
 Simple Code to Install/Update necessary modules for codes in this URL:
 https://taufiksutanto.blogspot.com/2018/01/easiest-social-media-analytics.html
 @author: Taufik Sutanto
-Notes: the code is given 'as is' & without warranty, you are responsible for your own action.
+Notes: the code is given 'as is' & without warranty, you are responsible for your own action
 """
 
 import subprocess
 
 if __name__ == "__main__":
-    dependencies = ['bs4','nltk','Sastrawi','scikit-learn','python-louvain','pyldavis','textblob','networkx']
+    dependencies = ['bs4','nltk','Sastrawi','scikit-learn','python-louvain','pyldavis','textblob','networkx','wordcloud']
     try:
         from tqdm import tqdm
     except:
