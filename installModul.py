c749cea8b7febe74343b7e67927cf706ed5c3851
Taufik Sutanto <taufikedys@users.noreply.github.com>
2018-01-11 18:31:36 +0700

    Create installModul.py

---
+++ installModul.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 15:03:38 2018
+Simple Code to Install/Update necessary modules for codes in this URL:
+https://taufiksutanto.blogspot.com/2018/01/easiest-social-media-analytics.html
+@author: Taufik Sutanto
+Notes: the code is given 'as is' & without warranty, you are responsible for your own action.
+"""
+
+import subprocess
+
+if __name__ == "__main__":
+    dependencies = ['bs4','nltk','Sastrawi','scikit-learn','python-louvain','pyldavis','textblob','networkx']
+    try:
+        from tqdm import tqdm
+    except:
+        subprocess.call(['pip', 'install', '-U', 'tqdm', '--upgrade'])    
+    print('Installing Modules (if necessary):')
+    for module in tqdm(dependencies):
+        try:
+            subprocess.call(['pip', 'install', '-U', module, '--upgrade'])
+        except:
+            pass
